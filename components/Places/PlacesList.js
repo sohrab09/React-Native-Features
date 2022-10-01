e780db767480d03017de9564fcf69b24589af828
sohrab09 <sohrab@nassagroup.org>
2022-10-01 18:58:34 +0600

    Adding header

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -29,7 +29,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#86c232'
     },
     fallbackText: {
         fontSize: 16,
